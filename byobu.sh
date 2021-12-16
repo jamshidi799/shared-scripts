f4e3bbcbd25ddad073cdcc6ce3431bf6f0fd8f9d
jamshidi.m799 <jamshidi.m799@gmail.com>
2021-12-16 13:39:56 +0330

    script for installing cisco vpn client

---
+++ byobu.sh
@@ -0,0 +1,7 @@
+sudo apt-get install byobu
+
+byobu-enable
+byobu-enable-prompt
+
+export BYOBU_RUN_DIR=$HOME/.byobu/run/
+byobu
---
+++ install-cisco-vpn-client.sh
@@ -0,0 +1,6 @@
+tar xvf (wget http://dl.nextadmin.net/dl2/Cisco%20AnyConnect%20Secure%20Mobility%20Client/4.10.x/anyconnect-linux64-4.10.01075-predeploy-k9.tar.gz)
+
+cd anyconnect-linux64-*/vpn
+sudo ./vpn_install.sh
+/opt/cisco/anyconnect/bin/vpn -v
+
