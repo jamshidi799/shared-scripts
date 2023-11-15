50b05a930a4f553cb36eb15283e24dfbe4519825
Mohammad Jamshidi <jamshidi.m799@gmail.com>
2023-11-15 18:19:15 +0330

    add zoxide

--- zellij.sh
+++ zellij.sh
@@ -27,3 +27,22 @@ https://github.com/eza-community/eza
 brew install eza
 echo "unalias ls
 alias ls='exa -al --color=always --group-directories-first'" >> ~/.zshrc
+
+# A smarter cd command. Supports all major shells.
+# https://github.com/ajeetdsouza/zoxide
+brew install zoxide
+echo "eval "$(zoxide init zsh)"" >> ~/.zshrc
+
+# tips:
+# z foo              # cd into highest ranked directory matching foo
+# z foo bar          # cd into highest ranked directory matching foo and bar
+# z foo /            # cd into a subdirectory starting with foo
+
+# z ~/foo            # z also works like a regular cd command
+# z foo/             # cd into relative path
+# z ..               # cd one level up
+# z -                # cd into previous directory
+
+# zi foo             # cd with interactive selection (using fzf)
+
+# z foo<SPACE><TAB>  # show interactive completions (zoxide v0.8.0+, bash 4.4+/fish/zsh only)
